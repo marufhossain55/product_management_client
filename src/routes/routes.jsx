a75b5cfc465c7055ecff4c819866575778aef30e
marufhossain55 <engrmaruf3@gmail.com>
2024-08-16 20:46:35 +0600

    dom added

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,6 +1,12 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Root from '../layouts/Root';
+import Home from '../Pages/Home';
 
 const router = createBrowserRouter([
-  { path: '/', element: <Root />, children [{index:true,element:}]},
+  {
+    path: '/',
+    element: <Root />,
+    children: [{ index: true, element: <Home /> }],
+  },
 ]);
+export default router;
