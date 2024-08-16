f59eeb59ac0b7e839e116085967cd3eec221e541
marufhossain55 <engrmaruf3@gmail.com>
2024-08-16 17:17:49 +0600

    first commit

---
+++ src/routes/routes.jsx
@@ -0,0 +1,6 @@
+import { createBrowserRouter } from 'react-router-dom';
+import Root from '../layouts/Root';
+
+const router = createBrowserRouter([
+  { path: '/', element: <Root />, children [{index:true,element:}]},
+]);
