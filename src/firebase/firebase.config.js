131222c00dcb9208cf1074b55184624e6cd414dc
marufhossain55 <engrmaruf3@gmail.com>
2024-08-17 18:38:40 +0600

    login registration added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDyB-0Cy3xWpX2RXE2e2_njJZEOhVEKOKY',
+  authDomain: 'productmanagement-21a2f.firebaseapp.com',
+  projectId: 'productmanagement-21a2f',
+  storageBucket: 'productmanagement-21a2f.appspot.com',
+  messagingSenderId: '387513894670',
+  appId: '1:387513894670:web:82387a845e826c91fa795f',
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
 import Footer from '../components/Footer';
+import Navbar from '../components/NavBar';
 
 const Root = () => {
   return (
     <div>
+      <Navbar />
       <Outlet />
       <Footer />
     </div>
