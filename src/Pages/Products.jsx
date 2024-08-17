c5190cd049180aa21a15cfd662ccb89fae44261c
marufhossain55 <engrmaruf3@gmail.com>
2024-08-17 16:20:04 +0600

    select by category done

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -6,28 +6,30 @@ const Products = () => {
   const [itemParPage, setItemParPage] = useState(6);
   const [count, setCount] = useState(0);
   const [products, setProducts] = useState([]);
+  const [filter, setFilter] = useState('');
 
   useEffect(() => {
     const getData = async () => {
       const { data } = await axios(
         `${
           import.meta.env.VITE_API_URL
-        }/all-products?page=${currentPage}&size=${itemParPage}`
+        }/all-products?page=${currentPage}&size=${itemParPage}&filter=${filter}`
       );
       setProducts(data);
     };
     getData();
-  }, [currentPage, itemParPage]);
+  }, [currentPage, itemParPage, filter]);
 
   useEffect(() => {
     const getCount = async () => {
       const { data } = await axios(
-        `${import.meta.env.VITE_API_URL}/products-count`
+        `${import.meta.env.VITE_API_URL}/products-count?filter=${filter}`
       );
+
       setCount(data.count);
     };
     getCount();
-  }, []);
+  }, [filter]);
 
   const numberOfPages = Math.ceil(count / itemParPage);
   console.log(numberOfPages);
@@ -38,6 +40,57 @@ const Products = () => {
   };
   return (
     <div>
+      <div className="flex flex-col md:flex-row justify-center items-center gap-5 ">
+        <div>
+          <select
+            onChange={(e) => {
+              setFilter(e.target.value);
+              setCurrentPage(1);
+            }}
+            value={filter}
+            name="category"
+            id="category"
+            className="border p-4 rounded-lg"
+          >
+            <option value="">Filter By Category</option>
+            <option value="men's clothing">men's clothing</option>
+            <option value="jewelery">jewelery</option>
+            <option value="electronics">electronics</option>
+            <option value="women's clothing">women's clothing</option>
+            <option value="men's footwear">men's footwear</option>
+            <option value="women's footwear">women's footwear</option>
+            <option value="home and kitchen">home and kitchen</option>
+          </select>
+        </div>
+
+        <form>
+          <div className="flex p-1 overflow-hidden border rounded-lg    focus-within:ring focus-within:ring-opacity-40 focus-within:border-blue-400 focus-within:ring-blue-300">
+            <input
+              className="px-6 py-2 text-gray-700 placeholder-gray-500 bg-white outline-none focus:placeholder-transparent"
+              type="text"
+              name="search"
+              placeholder="Enter Job Title"
+              aria-label="Enter Job Title"
+            />
+
+            <button className="px-1 md:px-4 py-3 text-sm font-medium tracking-wider text-gray-100 uppercase transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:bg-gray-600 focus:outline-none">
+              Search
+            </button>
+          </div>
+        </form>
+        <div>
+          <select
+            name="category"
+            id="category"
+            className="border p-4 rounded-md"
+          >
+            <option value="">Sort By Deadline</option>
+            <option value="dsc">Descending Order</option>
+            <option value="asc">Ascending Order</option>
+          </select>
+        </div>
+        <button className="btn">Reset</button>
+      </div>
       <div className="grid grid-cols-3 gap-4">
         {products?.map((product) => (
           <div
